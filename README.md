f277ed7d51b90ead2eb010ba2d4af576c0a00567
TopKek <braebear85@gmail.com>
2018-05-22 16:13:22 -0400

    Fix seeking issue where the client would update presence every second

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # discord-rhythmbox-plugin
-A simple plugin for rhythmbox to update your rich presence in discord
+A simple plugin for rhythmbox to update your rich presence in discord.
+This is based off of the built-in im-status plugin and has some borrowed code; credit where credit is due!
 
 ## Usage
 - Install `pypresence`:
+
 `pip install pypresence`
 (You can try it with sudo if it does not work)
 
 - Clone the repo:
+
 `git clone https://github.com/ToppleKek/discord-rhythmbox-plugin.git`
 
 - Put the `discord-rhythmbox-plugin` folder in `/usr/lib/rhythmbox/plugins`
 
 - Enable the plugin
+
--- discord-status.py
+++ discord-status.py
@@ -9,9 +9,11 @@ from pypresence import Presence
 class discord_status (GObject.Object, Peas.Activatable):
   RPC = Presence('415207119642689544')
   RPC.connect()
-  __gtype_name__ = 'IMStatusPlugin'
+  __gtype_name__ = 'DiscordStatusPlugin'
   object = GObject.property(type=GObject.Object)
-
+  start_date = None
+  playing_date = None
+  is_playing = False
   def __init__ (self):
     GObject.Object.__init__ (self)
 
@@ -66,21 +68,25 @@ class discord_status (GObject.Object, Peas.Activatable):
 
   def playing_entry_changed(self, sp, entry):
     if sp.get_playing_entry():
+      self.start_date = int(time.time())
+      self.playing_date = self.start_date
       info = self.get_info(sp)
       album = info[0]
       title = info[1]
       artist = info[2]
       duration = info[3]
       details="%s - %s" %(title, artist)
+      self.is_playing = True
 
       start_time = int(time.time())
       pos = sp.get_playing_time().time
       end_time = start_time + duration - pos
 
-      self.RPC.update(state=album, details=details[0:127], large_image="rhythmbox", small_image="play", small_text="Playing", start=start_time, end=end_time)
+      print("Updating RPC... (playing_entry_changed)")
+      self.RPC.update(state=album[0:127], details=details[0:127], large_image="rhythmbox", small_image="play", small_text="Playing", start=start_time, end=end_time)
 
   def playing_changed(self, sp, playing):
-    print("[discord-status] Playing chaned to: %s" %(playing))
+    print("Playing chaned to: %s" %(playing))
     album = None
     title = None
     artist = None
@@ -96,27 +102,38 @@ class discord_status (GObject.Object, Peas.Activatable):
       pos = sp.get_playing_time().time
       end_time = start_time + duration - pos
 
+    print("Updating RPC... (playing_changed)")
     if playing:
       print("Playing chaned to: playing")
-      self.RPC.update(state=album, details="%s - %s" %(title, artist)[0:127], large_image="rhythmbox", small_image="play", small_text="Playing", start=start_time, end=end_time)
+      self.is_playing = True
+      self.RPC.update(state=album[0:127], details=details[0:127], large_image="rhythmbox", small_image="play", small_text="Playing", start=start_time, end=end_time)
     elif not playing and not sp.get_playing_entry():
       print("Playing chaned to: stopped")
+      self.is_playing = False
       self.RPC.update(state="Playback Stopped", details="Rhythmbox Status Plugin", large_image="rhythmbox", small_image="stop", small_text="Stopped")
     else:
       print("Playing chaned to: paused")
-      self.RPC.update(state=album, details=details, large_image="rhythmbox", small_image="pause", small_text="Paused")
+      self.is_playing = False
+      self.RPC.update(state=album[0:127], details=details[0:127], large_image="rhythmbox", small_image="pause", small_text="Paused")
 
-  def elapsed_changed(self, sp, entry):
-    if sp.get_playing_entry():
-      info = self.get_info(sp)
-      album = info[0]
-      title = info[1]
-      artist = info[2]
-      duration = info[3]
-      details="%s - %s" %(title, artist)
+  def elapsed_changed(self, sp, elapsed):
+    print("[DEBUG] ELAPSED: %s" %(elapsed))
+    self.playing_date += 1
+    if self.playing_date - elapsed == self.start_date:
+      return
+    else:
+      if sp.get_playing_entry() and self.is_playing:
+        self.playing_date = self.start_date + elapsed
+        info = self.get_info(sp)
+        album = info[0]
+        title = info[1]
+        artist = info[2]
+        duration = info[3]
+        details="%s - %s" %(title, artist)
 
-      start_time = int(time.time())
-      pos = sp.get_playing_time().time
-      end_time = start_time + duration - pos
+        start_time = int(time.time())
+        pos = sp.get_playing_time().time
+        end_time = start_time + duration - pos
 
-      self.RPC.update(state=album, details=details[0:127], large_image="rhythmbox", small_image="play", small_text="Playing", start=start_time, end=end_time)
+        print("Updating RPC... (elapsed_changed)")
+        self.RPC.update(state=album[0:127], details=details[0:127], large_image="rhythmbox", small_image="play", small_text="Playing", start=start_time, end=end_time)
